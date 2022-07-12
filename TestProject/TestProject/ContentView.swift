59ffba819b29b0ae7c17988615e5c0971aa9ac6a
manati123 <silviu807@gmail.com>
2022-07-12 16:58:59 +0300

    feature: counter and some text

--- TestProject/TestProject/ContentView.swift
+++ TestProject/TestProject/ContentView.swift
@@ -8,9 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            Form{
+                Section{
+                    Text("Texty")
+                       
+                }
+            }
+            .navigationTitle("FirstUI")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        Button("Tap Count: \(tapCount)"){
+            tapCount += 1
+        }
+        
+//        Form{
+//            Section{
+//                Group{
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                }
+//            }
+//            Section{
+//                Group{
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                }
+//            }
+//            Section{
+//                Group{
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//                }
+//            }
+//
+//        }
     }
 }
 
