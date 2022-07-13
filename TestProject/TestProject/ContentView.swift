19ddd4bc669daa517375b7de92c39a49a85d614b
manati123 <silviu807@gmail.com>
2022-07-13 12:07:14 +0300

    feature: gionified app

--- TestProject/TestProject/ContentView.swift
+++ TestProject/TestProject/ContentView.swift
@@ -9,67 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     @State var tapCount = 0
+    @State var name = ""
+    @State var showAlert = false
+    @State private var selectionCase = "Happy Gion"
+    let gionutzi = ["Gigi Becali", "Happy Gion", "Sad Gion", "Chelutzu"]
+    
     var body: some View {
-        
         NavigationView{
             Form{
                 Section{
                     Text("Texty")
-                       
+                    Text("Texty")
+                    Text("Texty")
+                    Text("Texty")
+                    Text("Texty")
+                    Text("Texty")
                 }
             }
             .navigationTitle("FirstUI")
             .navigationBarTitleDisplayMode(.inline)
-        }
-        Button("Tap Count: \(tapCount)"){
-            tapCount += 1
+            
+            
         }
         
-//        Form{
-//            Section{
-//                Group{
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                }
-//            }
-//            Section{
-//                Group{
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                }
-//            }
-//            Section{
-//                Group{
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                Text("Hello, world!")
-//                }
-//            }
-//
-//        }
+        Form{
+            Section{
+                TextField("Your name here...",text: $name){
+                    
+                }
+            }
+        }
+        
+        Form{
+            Picker("Choose your fighter",selection: $selectionCase){
+                ForEach(gionutzi, id: \.self){
+                    Text("\($0)")
+                }
+            }
+        }
+        Section{
+            Button("Tap Count: \(tapCount)"){
+                tapCount += 1
+                if tapCount % 10 == 0{
+                    self.name = "Hatz GIONULE"
+                    self.showAlert = true
+                }
+            }
+            .alert(isPresented: $showAlert){
+                Alert(title: Text("You clicked 10xsmth times WOOOW"),
+                      message: Text("LMAO"),
+                      dismissButton: .default(Text("Mersi Gionutzule")))
+                
+            }
+        }
+        
+        
+
     }
 }
 
