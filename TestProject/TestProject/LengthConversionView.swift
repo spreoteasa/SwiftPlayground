3862a1a1516d2308bddb44a5afdb6fd2f0d41f49
manati123 <silviu807@gmail.com>
2022-07-14 15:38:13 +0300

    feature: finished temperature converter

--- TestProject/TestProject/LengthConversionView.swift
+++ TestProject/TestProject/LengthConversionView.swift
@@ -14,51 +14,51 @@ struct LengthConversionView: View {
     @State var lengthConverter: LengthConverter = LengthConverter()
     @State var showAlert: Bool = false
     var body: some View {
-        NavigationView{
-            Form{
-                Section{
-                    Picker("Initial State", selection: $initialLengthType){
-                        ForEach(LengthType.allCases,id: \.self){ lengthType in
-                            Text(lengthType.rawValue)
-                            
+            NavigationView{
+                Form{
+                    Section{
+                        Picker("Initial State", selection: $initialLengthType){
+                            ForEach(LengthType.allCases,id: \.self){ lengthType in
+                                Text(lengthType.rawValue)
+                                
+                            }
                         }
+                    } header:{
+                        Text("What is the base length type?")
                     }
-                } header:{
-                    Text("What is the base length type?")
-                }
-                Section{
-                    TextField("",value: $lengthValue,
-                              format: .number)
-                        .keyboardType(.numberPad)
-                } header:{
-                    Text("What length value do you want to convert?")
-                }
-                Section{
-                    Picker("Final State", selection: $finalLengthType){
-                        ForEach(LengthType.allCases,id: \.self){ lengthType in
-                            Text(lengthType.rawValue)
-                            
+                    Section{
+                        TextField("",value: $lengthValue,
+                                  format: .number)
+                            .keyboardType(.numberPad)
+                    } header:{
+                        Text("What length value do you want to convert?")
+                    }
+                    Section{
+                        Picker("Final State", selection: $finalLengthType){
+                            ForEach(LengthType.allCases,id: \.self){ lengthType in
+                                Text(lengthType.rawValue)
+                                
+                            }
                         }
+                    } header:{
+                        Text("What is the final length type?")
                     }
-                } header:{
-                    Text("What is the final length type?")
+                    
+                    Button("Convert"){
+                        self.lengthConverter.length = self.lengthValue
+                        self.lengthConverter.initialLengthType = self.initialLengthType
+                        self.lengthConverter.finalLengthType = self.finalLengthType
+                        self.showAlert = true
+                    }
+                    .alert(isPresented: $showAlert){
+                            Alert(title: Text("Conversion result:"),
+                                    message: Text((self.lengthConverter.compute())),
+                                    dismissButton: .default(Text("OK")))
                 }
-                
-                Button("Convert"){
-                    self.lengthConverter.length = self.lengthValue
-                    self.lengthConverter.initialLengthType = self.initialLengthType
-                    self.lengthConverter.finalLengthType = self.finalLengthType
-                    self.showAlert = true
-                }
-                .alert(isPresented: $showAlert){
-                        Alert(title: Text("Conversion result:"),
-                                message: Text((self.lengthConverter.compute())),
-                                dismissButton: .default(Text("OK")))
+                .navigationTitle("Length Converter")
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .navigationTitle("Length Converter")
-            .navigationBarTitleDisplayMode(.inline)
         }
-    }
     }
 }
 
--- TestProject/TestProject/TemperatureConversionView.swift
+++ TestProject/TestProject/TemperatureConversionView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct TemperatureConversionView: View {
+    @State var temperature: Double = 0.0
+    @State var tempConverter: TemperatureConverter = TemperatureConverter()
+    @State var initialTempType: TemperatureType = .celsius
+    @State var showAlert: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            Form{
+                Section{
+                    Picker("Initial State", selection: $initialTempType){
+                        ForEach(TemperatureType.allCases,id: \.self){ tempType in
+                            Text(tempType.rawValue)
+                            
+                        }
+                    }
+                } header:{
+                    Text("What is the base temperature type?")
+                }
+                Section{
+                    TextField("",value: $temperature,
+                              format: .number)
+                        .keyboardType(.numberPad)
+                } header:{
+                    Text("What temperature value do you want to convert?")
+                }
+                
+                Button("Convert"){
+                    self.tempConverter.temperature = self.temperature
+                    self.tempConverter.initialType = self.initialTempType
+                    self.showAlert = true
+                }
+                .alert(isPresented: $showAlert){
+                        Alert(title: Text("Conversion result:"),
+                                message: Text((self.tempConverter.convert())),
+                                dismissButton: .default(Text("OK")))
+            }
+            .navigationTitle("Temperature Converter")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
     }
 }
 
