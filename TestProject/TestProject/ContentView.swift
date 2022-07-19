bb9281f64a13c3f97ebd880ac42b4c33ab87fea2
manati123 <silviu807@gmail.com>
2022-07-19 14:42:11 +0300

    feature: added buttons and minimal functionality

--- TestProject/TestProject/ContentView.swift
+++ TestProject/TestProject/ContentView.swift
@@ -8,21 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State var correctAnswer = Int.random(in: 0...2)
+    @State private var showScore = false
+    @State private var scoreTitle = ""
     var body: some View {
-        VStack{
-            Text("Tap the flag of:")
-            Text(countries[correctAnswer])
-            ForEach(0..<3){number in
-                Button{
-                    //Something happened here
-                } label:{
-                    Image(countries[number])
-                        .renderingMode(.original)
+        ZStack{
+            Color(red: 0.5, green: 1, blue: 0.8)
+                .ignoresSafeArea()
+            VStack(spacing: 30){
+                
+                VStack{
+                    Text("Tap the flag of:")
+                    Text(countries[correctAnswer])
+                }
+                ForEach(0..<3){number in
+                    Button{
+                        print("The flag of \(countries[number]) was clicked")
+                        flagTapped(number)
+                    } label:{
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
                 }
             }
         }
+    }
+    func flagTapped(_ chosenFlag: Int) {
+        if chosenFlag == self.correctAnswer {
+            print("Correct")
+        }
+        else {
+            print("Wrong")
+        }
+        self.showScore = true
+    }
+    
+    func askQuestion(){
+        self.countries.shuffle()
+        self.correctAnswer = Int.random(in: 0...2)
     }
     
 }
