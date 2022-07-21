4b6ed3d389a0930ca60a2bf093261f4bc08999dc
manati123 <silviu807@gmail.com>
2022-07-21 12:44:31 +0300

    feature: finished functionality

--- TestProject/TestProject/ContentView.swift
+++ TestProject/TestProject/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingMessage = false
     var body: some View {
         NavigationView {
             List{
@@ -18,6 +21,12 @@ struct ContentView: View {
                     TextField("Insert your word", text: $newWord)
                         .textInputAutocapitalization(.never)
                         .onSubmit(addNewWord)
+                        .onAppear(perform: startGame)
+                        .alert(errorTitle,isPresented: $showingMessage) {
+                            Button("OK", role: .cancel) { }
+                        } message: {
+                            Text(errorMessage)
+                        }
                 }
                 
                 Section {
@@ -29,17 +38,44 @@ struct ContentView: View {
                     }
                 }
             }
+            .navigationTitle(rootWord)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .navigationTitle(rootWord)
+        
+    }
+    
+    func insertWordConditions(word: String) -> Bool {
+        return spellChecker(word: word) == true && word.count > 0 && !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
     }
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard answer.count > 0 else { return } //checking if the word is empty
+        //check if word complies to the rules
+        guard insertWordConditions(word: answer) == true else {
+            wordError(title: "Not real / misspelled / impossible / already used", message: "Dumbooo")
+            return
+        }
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
+    }
+    
+    func isOriginal(word: String) -> Bool{
+        return usedWords.contains(word)
     }
     
     func spellChecker(word: String) -> Bool {
@@ -51,6 +87,24 @@ struct ContentView: View {
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         //return wether or not the user comitted an oopsie
         return misspelledRange.location == NSNotFound
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "sternocleidomastoidian"
+                
+                return
+            }
+        }
+        fatalError("Could not load start file from bundle!")
+    }
+    
+    func wordError(title: String, message: String){
+        errorTitle = title
+        errorMessage = message
+        showingMessage = true
     }
 }
 
