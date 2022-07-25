95e19b317790da9e917dc32d6faf7ad7df30b544
manati123 <silviu807@gmail.com>
2022-07-25 13:22:09 +0300

    first commit

--- TestProject/TestProject/Views/ContentView.swift
+++ TestProject/TestProject/Views/ContentView.swift
@@ -8,48 +8,8 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 0
-    @State private var animationAmount = 0.0
-    
-    var editableView: some View {
-        NavigationView{
-            VStack {
-                List {
-                    ForEach(numbers, id:\.self) {
-                        Text("Row \($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                
-                
-                Button("Add number") {
-                    numbers.append(currentNumber)
-                    currentNumber += 1
-                }
-                .padding(30)
-            }
-            .toolbar {
-                EditButton()
-            }
-        }
-    }
-    
     var body: some View {
-        Button("Tap me") {
-            withAnimation {
-                animationAmount += 360
-            }
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
-    }
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
+        Text("kjsdnf")
     }
 }
 
