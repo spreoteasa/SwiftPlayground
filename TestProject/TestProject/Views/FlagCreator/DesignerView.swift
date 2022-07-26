18ca07ca365349041488840e943c12bcfc0d0369
Preoteasa Ioan-Silviu <preoteasaioan-silviu@HUAWEI_P9_lite_2017-f3ac3.tapptitude>
2022-07-26 13:22:47 +0300

    feature: most UI done

---
+++ TestProject/TestProject/Views/FlagCreator/DesignerView.swift
@@ -0,0 +1,170 @@
+//
+//  ColorPickerView.swift
+//  TestProject
+//
+//  Created by Preoteasa Ioan-Silviu on 26.07.2022.
+//
+
+import SwiftUI
+
+struct DesignerView: View {
+    var body: some View {
+        ZStack {
+            MainBackground()
+            VStack(spacing: 20){
+                ZStack{
+                    
+                    BackgroundRectangle()
+                    PickerSection()
+                }
+                CustomButton(backgroundColor: "BlueFontColor",buttonText: "Add Stripe")
+                AddSubsectionSection()
+                CustomButton(backgroundColor: "LightGreen", buttonText: "Commit Section")
+            }
+        }
+    }
+}
+
+struct PickerSection: View {
+    var body: some View {
+        HStack{
+            
+            Spacer()
+            VStack{
+                Text("PICK COLOR")
+                    .fontWeight(.semibold)
+                ColorPickerButton()
+            }
+            Spacer()
+            VStack{
+                Text("PICK EMBLEM")
+                    .fontWeight(.semibold)
+                EmblemPickerButton()
+            }
+            Spacer()
+            
+        }.frame(width:300, height: 100)
+    }
+}
+
+struct BackgroundRectangle: View {
+    var body: some View {
+        Rectangle()
+            .frame(width: 300, height: 100)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            .shadow(color: .gray, radius: 3, x: 0, y: 0)
+    }
+}
+
+struct EmblemPickerButton: View {
+    var body: some View {
+        Button {}
+    label: {
+        Image(systemName: "photo.on.rectangle")
+    }.frame(width: 100, height: 25)
+            .foregroundColor(Color("LightGray"))
+    }
+}
+
+
+struct ColorPickerButton: View {
+    var body: some View {
+        Button("") {}
+            .frame(width: 100, height: 25)
+            .background(Color("LightGray"))
+            .clipShape(RoundedRectangle(cornerRadius: 2))
+    }
+}
+
+struct CustomButton: View {
+    var backgroundColor: String = ""
+    var buttonText: String = ""
+    var body: some View {
+        Button {
+            print("hehe")
+        }
+    label: {
+        Text(buttonText)
+            .foregroundColor(.white)
+            .fontWeight(.semibold)
+            .font(.custom("SF Pro Text", fixedSize: 24))
+    }
+        
+    .frame(width: 300, height: 50)
+    .background(Color(backgroundColor))
+    .clipShape(RoundedRectangle(cornerRadius: 5))
+        //    .shadow(color: .gray, radius: 3, x: 0, y: 0)
+        
+        
+    }
+}
+
+struct AddSubsectionSection: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .frame(width: 300, height: 100)
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+                .shadow(color: .gray, radius: 3, x: 0, y: 0)
+            VStack {
+                Text("Add Stripe")
+                    .foregroundColor(Color("BlueFontColor"))
+                    .fontWeight(.bold)
+                    
+                HStack{
+                    Spacer()
+                    Button { }
+                label: {
+                    Image(systemName: "rectangle.grid.1x2.fill")
+                        .resizable()
+                        .frame(width:20,height:20)
+                        .foregroundColor(Color("BlueFontColor"))
+                        .rotationEffect(.degrees(90))
+                        
+                    
+                }
+                    Spacer()
+                    Image(systemName: "line.diagonal")
+                        .resizable()
+                        .frame(width:5, height:40)
+                        .rotationEffect(.degrees(173))
+                        .foregroundColor(Color("LightGray"))
+                    Spacer()
+                    Button { }
+                label: {
+                    Image(systemName: "rectangle.grid.1x2.fill")
+                        .resizable()
+                        .frame(width:20,height:20)
+                        .foregroundColor(Color("BlueFontColor"))
+                        
+                }
+                    Spacer()
+                }.frame(width:300)
+            }
+        }
+    }
+}
+
+struct MainBackground: View {
+    var body: some View {
+        GeometryReader {geometry in
+            Rectangle()
+            
+                .frame(width:geometry.size.width * 1,
+                       height: geometry.size.height * 1)
+                .foregroundColor(Color("LightGray"))
+            
+            
+        }
+        
+    }
+}
+
+
+struct DesignerView_Previews: PreviewProvider {
+    static var previews: some View {
+        DesignerView()
+    }
+}
--- TestProject/TestProject/Views/FlagCreator/FlagStackView.swift
+++ TestProject/TestProject/Views/FlagCreator/FlagStackView.swift
@@ -9,17 +9,41 @@ import SwiftUI
 
 struct FlagStackView: View {
     var body: some View {
-        SaveFlagButton()
+        VStack(alignment: .trailing, spacing: 40) {
+            HStack{
+                Spacer()
+                SaveFlagButton()
+            }
+            HStack{
+                Spacer()
+            FlagCreationZoneView()
+                Spacer()
+            }
+            Spacer()
+        }
+        .frame(height: 300)
     }
 }
 
 struct SaveFlagButton: View {
     var body: some View {
-        Button("Save Flag") { }
+        Button("Save Flag", action: saveFlag)
+            .frame(width: 90, height: 40)
             .background(Color("SaveFlagBackgroundBlue"))
             .foregroundColor(Color("BlueFontColor"))
             .clipShape(RoundedRectangle(cornerRadius: 5))
-            .frame(width: 100, height: 80)
+    }
+    
+    func saveFlag() {
+        print("foo")
+    }
+}
+
+struct FlagCreationZoneView: View {
+    var body: some View {
+        Rectangle()
+            .frame(width: 300, height: 150, alignment: .center)
+            
     }
 }
 
