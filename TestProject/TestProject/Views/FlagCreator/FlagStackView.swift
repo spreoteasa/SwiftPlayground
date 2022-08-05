ccd423a1db81bbf78e563fc4f72b637295034241
manati123 <silviu807@gmail.com>
2022-08-05 13:36:34 +0300

    feature: taking pics of the flags

--- TestProject/TestProject/Views/FlagCreator/FlagStackView.swift
+++ TestProject/TestProject/Views/FlagCreator/FlagStackView.swift
@@ -21,10 +21,6 @@ struct FlagStackView: View {
                 HStack{
                     Spacer()
                     viewModel.getWholeView()
-                        .onTapGesture {
-                            let image = self.snapshot()
-                            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-                        }
                     Spacer()
                 
                 }.frame(height: geometry.size.height * 0.7)
@@ -40,39 +36,23 @@ struct FlagStackView: View {
 }
 
 extension View {
-    func snapshot() -> UIImage {
-        let controller = UIHostingController(rootView: self)
-        let view = controller.view
-        
-        let targetSize = controller.view.intrinsicContentSize
-        view?.bounds = CGRect(origin: .zero, size: targetSize)
-        view?.backgroundColor = .clear
-        
-        let renderer = UIGraphicsImageRenderer(size: targetSize)
-        
-        return renderer.image { _ in
-            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
-        }
-    }
-    
-    func asUiImage() -> UIImage {
-        var uiImage = UIImage(systemName: "exclamationmark.triangle.fill")!
-        let controller = UIHostingController(rootView: self)
-        
-        if let view = controller.view {
-            let contentSize = view.intrinsicContentSize
-            view.bounds = CGRect(origin: .zero, size: contentSize)
-            view.backgroundColor = .clear
-            
-            let renderer = UIGraphicsImageRenderer(size: contentSize)
-            uiImage = renderer.image { _ in
-                view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
-            }
-        }
-        return uiImage
-    }
+  func saveAsImage(width: CGFloat, height: CGFloat, _ completion: @escaping (UIImage) -> Void) {
+    let size = CGSize(width: width, height: height)
+    let controller = UIHostingController(rootView: self.frame(width: width, height: height))
+//    controller.view.bounds = CGRect(origin: .zero, size: size)
+    controller.view.bounds = CGRect(x: 0, y: -10, width: width, height: height + 50)
+    let image = controller.view.asImage()
+    completion(image)
+  }
 }
-
+extension UIView {
+  func asImage() -> UIImage {
+    let renderer = UIGraphicsImageRenderer(size: self.bounds.size)
+    return renderer.image { ctx in
+      self.drawHierarchy(in: self.bounds, afterScreenUpdates: true)
+    }
+  }
+}
 
 
 struct SaveFlagButton: View {
@@ -86,7 +66,27 @@ struct SaveFlagButton: View {
     }
     
     func saveFlag() {
-        let image = viewModel.getWholeView().asUiImage()
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        let image = viewModel.getWholeView().saveAsImage(width: 300, height: 150) { image in
+            let sideLength = min(
+                image.size.width,
+                image.size.height - 65
+            )
+            let sourceSize = image.size
+            let xOffset = 8.0
+            let yOffset = 74.0
+            
+            let cropRect = CGRect(
+                x: xOffset,
+                y: yOffset,
+                width: 3000.0,
+                height: 300.0
+            ).integral
+            let sourceCGImage = image.cgImage!
+            let croppedCGImage = sourceCGImage.cropping(
+                to: cropRect
+            )!
+            UIImageWriteToSavedPhotosAlbum(UIImage(cgImage: croppedCGImage), nil, nil, nil)
+        }
+//        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
     }
 }
